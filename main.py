061c2bba641122bc35099d51245e1939267b0ad6
RishiVikram <rishi.vikram.1@gmail.com>
2020-04-04 18:11:10 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import (QApplication, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget, QFileDialog, QMessageBox, QSplashScreen)
+from PyQt5.QtWidgets import (QApplication, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget, QFileDialog, QMessageBox, QSplashScreen,QMenuBar,QStatusBar,QMainWindow)
 from PyQt5.QtGui import *
 from PyQt5.QtChart import *
 from PyQt5.QtCore import *
+from PyQt5 import QtCore, QtGui
 import csv
 import sys
 import time
+import xlsxwriter
+from xlrd import open_workbook
 
 class WidgetGallery(QDialog):
     def __init__(self, parent=None):
@@ -26,14 +29,19 @@ class WidgetGallery(QDialog):
         self.dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
         self.dark_palette.setColor(QPalette.HighlightedText, QColor(0, 0, 0))
         self.source_df_dict={}
+        self.source_df_module_name={}
+        self.source_df_error={}
         self.target_df_dict={}
         self.target_df_modulename={}
         self.target_df_error={}
         self.sourcefilename = ''
         self.targetfilename = ''
+        self.modified={} 
+        self.same={}
+        self.added={}
         QApplication.setPalette(self.dark_palette)
         QApplication.setStyle("Fusion")
-        #self.ShowSplashscreen()
+
         self.MainScreenLayout()
 
         mainLayout = QGridLayout()
@@ -44,17 +52,6 @@ class WidgetGallery(QDialog):
         mainLayout.setColumnStretch(1, 2)
         self.setLayout(mainLayout)
 
-    def ShowSplashscreen(self):
-        splash_pix = QPixmap('DD.png')
-        splash = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
-        splash.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
-        splash.setEnabled(False)
-        splash.setMask(splash_pix.mask())
-        splash.show()
-        timer = QElapsedTimer()
-        timer.start()
-        while timer.elapsed() < 2000 :
-            pass
     def MainScreenLayout(self):
         #Groupbox
         self.sourcegroupbox = QGroupBox("Choose the Source File")
@@ -106,23 +103,71 @@ class WidgetGallery(QDialog):
     def ExportResultsInCSV(self):
         if(self.CSVDataPreprocessor()):
             return 
-        exportfilename,_ = QFileDialog.getSaveFileName(self,"Save Result File","","CSV Files (*.csv)")
-        with open(exportfilename, 'w', newline='') as output_file:
-            fieldnames=['ScenarioName','ScenarioRunStatus','ModuleName','Error']
-            writer = csv.DictWriter(output_file,fieldnames=fieldnames)
-            writer.writeheader()
-            
-            for item in self.modified:
-                writer.writerow({'ScenarioName':item,'ScenarioRunStatus':self.target_df_dict[item],'ModuleName':self.target_df_modulename[item],'Error':self.target_df_error[item]})
+        
+        self.StatusCounter()
 
+        exportfilename,_ = QFileDialog.getSaveFileName(self,"Save Result File","","Excel Files (*.xlsx)")
+        
+        workbook= xlsxwriter.Workbook(exportfilename)
+        deltaworksheet=workbook.add_worksheet('Delta Report')
+        failedworksheet=workbook.add_worksheet('Failed In Both Run')
+        passedworksheet=workbook.add_worksheet('Passed In Both Run')
+        row=0
+        col=0
+
+        deltaworksheet.write(row,col,'ScenarioName')
+        deltaworksheet.write(row,col+1,'ScenarioRunStatus')
+        deltaworksheet.write(row,col+2,'ModuleName')
+        deltaworksheet.write(row,col+3,'Error')
+        row+=1
+        for item in self.modified.keys():
+            deltaworksheet.write(row,col,item)
+            deltaworksheet.write(row,col+1,self.target_df_dict[item])
+            deltaworksheet.write(row,col+2,self.target_df_modulename[item])
+            deltaworksheet.write(row,col+3,self.target_df_error[item])
+            row+=1
+
+        row=0
+        col=0
+
+        failedworksheet.write(row,col,'ScenarioName')
+        failedworksheet.write(row,col+1,'ScenarioRunStatus')
+        failedworksheet.write(row,col+2,'ModuleName')
+        failedworksheet.write(row,col+3,'Error')
+        row+=1
+        for item in self.same_failed_dict.keys():
+            failedworksheet.write(row,col,item)
+            failedworksheet.write(row,col+1,self.source_df_dict[item])
+            failedworksheet.write(row,col+2,self.source_df_module_name[item])
+            failedworksheet.write(row,col+3,self.source_df_error[item])
+            row+=1
+
+        row=0
+        col=0
+
+        passedworksheet.write(row,col,'ScenarioName')
+        passedworksheet.write(row,col+1,'ScenarioRunStatus')
+        passedworksheet.write(row,col+2,'ModuleName')
+        passedworksheet.write(row,col+3,'Error')
+        row+=1
+        for item in self.same_success_dict.keys():
+            passedworksheet.write(row,col,item)
+            passedworksheet.write(row,col+1,self.source_df_dict[item])
+            passedworksheet.write(row,col+2,self.source_df_module_name[item])
+            passedworksheet.write(row,col+3,self.source_df_error[item])
+            row+=1
+
+        workbook.close()
+        
     def CSVComparer(self):
         source_keys = set(self.source_df_dict.keys())
         target_keys = set(self.target_df_dict.keys())
         intersect_keys = source_keys.intersection(target_keys)
         added = source_keys - target_keys
         removed = target_keys - source_keys
+        added = {i:self.source_df_dict[i] for i in added}
         modified = {i : (self.source_df_dict[i], self.target_df_dict[i]) for i in intersect_keys if self.source_df_dict[i] != self.target_df_dict[i]}
-        same = set(i for i in intersect_keys if self.source_df_dict[i] == self.target_df_dict[i])
+        same = {i: (self.source_df_dict[i], self.target_df_dict[i]) for i in intersect_keys if self.source_df_dict[i] == self.target_df_dict[i]}
         return added, removed, modified, same
 
     def CSVDataPreprocessor(self):
@@ -139,6 +184,8 @@ class WidgetGallery(QDialog):
                     csv_reader = csv.DictReader(csv_file,delimiter='\t')
                     for row in csv_reader:
                         self.source_df_dict.update(dict({row["ScenarioName"]:row["ScenarioRunStatus"]}))
+                        self.source_df_module_name.update(dict({row["ScenarioName"]:row["ModuleName"]}))
+                        self.source_df_error.update(dict({row['ScenarioName']:row["Error"]}))
                     break
             except OverflowError:
                 maxInt = int(maxInt/10)
@@ -158,53 +205,224 @@ class WidgetGallery(QDialog):
         self.added, self.removed, self.modified, self.same = self.CSVComparer()
     
     def StatusCounter(self):
+
         self.success=0
         self.failed=0
-        self.running=0
+        self.same_success=0
+        self.same_failed=0
+        self.source_others=0
+        self.target_others=0
+
+        self.source_success_dict={}
+        self.source_failed_dict={}
+
+        self.same_success_dict={}
+        self.same_failed_dict={}
+
+            
+        for item in self.source_df_dict.keys():
+            try:
+                if str(self.source_df_dict[item]) == 'Completed Sucessfully':
+                    self.source_success_dict.update(dict({item:self.source_df_dict[item]}))
+                elif str(self.source_df_dict[item]) == 'Completed With Errors':
+                    self.source_failed_dict.update(dict({item:self.source_df_dict[item]}))
+                else:
+                    self.source_others+=1
+            except KeyError as error:
+                print(error)
+        
         for item in self.modified.keys():
             if str(self.target_df_dict[item]) == 'Completed Sucessfully':
                 self.success+=1
             elif str(self.target_df_dict[item]) == 'Completed With Errors':
                 self.failed+=1
-            elif str(self.target_df_dict[item]) == 'Running':
-                self.running+=1
+            else:
+                self.target_others+=1
+        
+        for item in self.added:
+            if str(self.source_df_dict[item]) == 'Completed Sucessfully':
+                self.same_success+=1
+            elif str(self.source_df_dict[item]) == 'Completed With Errors':
+                self.same_failed+=1
+            else:
+                self.target_others+=1
+            
+        for item in self.same.keys():
+            if str(self.source_df_dict[item]) == 'Completed Sucessfully':
+                self.same_success+=1
+                self.same_success_dict.update(dict({item:self.source_df_dict[item]}))
+            elif str(self.source_df_dict[item]) == 'Completed With Errors':
+                self.same_failed+=1
+                self.same_failed_dict.update(dict({item:self.source_df_dict[item]}))
+            else:
+                self.target_others+=1
+        
+        return
+
     def CreateChart(self):
         if(self.CSVDataPreprocessor()):
-            return
+           return
         self.StatusCounter()
         if self.success == 0 or self.failed==0:
             alert = QMessageBox.warning(self,'Alert',"No Difference Between Both Reports",QMessageBox.Ok)
             return
-        data = {
-        "Success=="+str(self.success): (self.success, QColor("green")),
-        "Failed=="+str(self.failed): (self.failed, QColor("red")),
-        "Running=="+str(self.running): (self.running, QColor("yellow"))
-        }
-
-        series = QPieSeries()
-        
-        for name, (value, color) in data.items():
-            _slice = series.append(name, value)
-            _slice.setBrush(color)
-
-        chart = QChart()
-        
-        chart.legend().setAlignment(Qt.AlignBottom)
-        chart.setAnimationOptions(QChart.SeriesAnimations)
-        chart.setTheme(QChart.ChartThemeDark)
-        chart.addSeries(series)
-        chartview = QChartView(chart)
-        chartview.setRenderHint(QPainter.Antialiasing)
-        
-
-        self.chartdialog = QDialog()
-        self.chartdialog.setWindowTitle("Regression Comparison Chart")
-        
-        chartlayout = QHBoxLayout()
-        chartlayout.addWidget(chartview)
-        self.chartdialog.setLayout(chartlayout)
-        self.chartdialog.resize(500,500)
+        self.chartdialog = ChartDialog(self)
         self.chartdialog.show()
+
+#Classes for Chart UI
+class Ui_MainWindow(object):
+    def setupUi(self, Dialog):
+        Dialog.resize(1600, 500)
+        self.centralwidget = QWidget(Dialog)
+        self.centralwidget.setWindowTitle("Regression Comparison chart")
+
+        oldregressionlabel=QLabel(self.centralwidget)
+        oldregressionlabel.setText("Old Regression Run")
+        oldregressionlabel.move(60,80)
+
+        newregressionlabel = QLabel(self.centralwidget)
+        newregressionlabel.setText("New Regression Run")
+        newregressionlabel.move(60,220)
+        
+        legend_failed = QLabel(self.centralwidget)
+        legend_failed.setText("Failed Scenarios")
+        legend_failed.move(370,395)
+
+        
+        legend_success = QLabel(self.centralwidget)
+        legend_success.setText("Passed Scenarios")
+        legend_success.move(520,395)
+
+        legend_others =QLabel(self.centralwidget)
+        legend_others.setText("Others")
+        legend_others.move(680,395)
+
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+class ChartDialog(WidgetGallery):
+    def __init__(self,widgetgallery, parent=None):
+        QDialog.__init__(self, parent)
+        self.ui = Ui_MainWindow()  
+        self.ui.setupUi(self)
+        self.Source_Success_dict=widgetgallery.source_success_dict
+        self.Source_Failed_dict=widgetgallery.source_failed_dict
+
+        self.success=widgetgallery.success
+        self.same_success=widgetgallery.same_success
+        self.failed=widgetgallery.failed
+        self.same_failed=widgetgallery.same_failed
+        self.source_others=widgetgallery.source_others
+        self.target_others=widgetgallery.target_others
+    
+    #ChartDrawer
+    def paintEvent(self, event):
+        screenwidth=700
+        factor=1600
+        deltadistance = 270
+        painter = QPainter(self)
+        pen = QPen()
+        pen.setWidth(3)
+        #Old Regression Run
+        if len(self.Source_Failed_dict) != 0:
+            pen.setColor(QColor(Qt.darkRed))
+            painter.setPen(pen)
+            painter.setBrush(QColor(Qt.darkRed))
+            painter.drawRoundedRect(40+deltadistance, 40, (len(self.Source_Failed_dict))/screenwidth * factor, 100, 10, 10)
+            pen.setColor(QColor(Qt.white))
+            painter.setPen(pen)
+            painter.setBrush(QColor(Qt.white))
+            painter.drawText(QRect(40+deltadistance, 40, (len(self.Source_Failed_dict))/screenwidth * factor, 100),Qt.AlignCenter,str(len(self.Source_Failed_dict)))
+            
+           
+        if len(self.Source_Success_dict)!=0:
+            pen.setColor(QColor(Qt.darkGreen))
+            painter.setPen(pen)
+            painter.setBrush(QColor(Qt.darkGreen))
+            painter.drawRoundedRect(40+deltadistance+(len(self.Source_Failed_dict))/screenwidth * factor+30, 40, (len(self.Source_Success_dict)/screenwidth)*factor, 100, 10, 10)
+            pen.setColor(QColor(Qt.white))
+            painter.setPen(pen)
+            painter.setBrush(QColor(Qt.white))
+            painter.drawText(QRect(40+deltadistance+(len(self.Source_Failed_dict))/screenwidth * factor+30, 40, (len(self.Source_Success_dict)/screenwidth)*factor, 100),Qt.AlignCenter,str(len(self.Source_Success_dict)))
+        
+        if self.source_others !=0:
+            pen.setColor(QColor(Qt.darkBlue))
+            painter.setPen(pen)
+            painter.setBrush(QColor(Qt.darkBlue))
+            painter.drawRoundedRect(40+deltadistance+(len(self.Source_Failed_dict))/screenwidth * factor+30+(len(self.Source_Success_dict)/screenwidth)*factor+30,40,(self.source_others/screenwidth)*factor,100,10,10)
+            pen.setColor(QColor(Qt.white))
+            painter.setPen(pen)
+            painter.setBrush(QColor(Qt.white))
+            painter.drawText(QRect(40+deltadistance+(len(self.Source_Failed_dict))/screenwidth * factor+30+(len(self.Source_Success_dict)/screenwidth)*factor+30,40,(self.source_others/screenwidth)*factor,100),Qt.AlignCenter,str(self.source_others))
+
+        #New Regression Run
+        if self.success!=0:
+            pen.setColor(QColor(Qt.darkGreen))
+            painter.setPen(pen)
+            painter.setBrush(QColor(Qt.darkGreen))
+            painter.drawRoundedRect(40+deltadistance, 180, (self.success/screenwidth) * factor, 100, 10, 10)
+            pen.setColor(QColor(Qt.white))
+            painter.setPen(pen)
+            painter.setBrush(QColor(Qt.white))
+            painter.drawText(QRect(40+deltadistance, 180, (self.success/screenwidth) * factor, 100),Qt.AlignCenter,str(self.success))
+        distance1=40+deltadistance+((self.success/screenwidth) * factor)+10
+        if self.same_failed!=0:
+            pen.setColor(QColor(Qt.darkRed))
+            painter.setPen(pen)
+            painter.setBrush(QColor(Qt.darkRed))
+            painter.drawRoundedRect(distance1, 180, (self.same_failed/screenwidth) * factor, 100, 10, 10)
+            pen.setColor(QColor(Qt.white))
+            painter.setPen(pen)
+            painter.setBrush(QColor(Qt.white))
+            painter.drawText(QRect(distance1, 180, (self.same_failed/screenwidth) * factor, 100),Qt.AlignCenter,str(self.same_failed))
+        distance2= (abs(len(self.Source_Success_dict)-len(self.Source_Failed_dict))/2)+deltadistance
+        if self.same_success!=0:
+            pen.setColor(QColor(Qt.darkGreen))
+            painter.setPen(pen)
+            painter.setBrush(QColor(Qt.darkGreen))
+            painter.drawRoundedRect(40+deltadistance+(len(self.Source_Failed_dict))/screenwidth * factor+30, 180, (self.same_success/screenwidth) * factor, 100, 10, 10)
+            pen.setColor(QColor(Qt.white))
+            painter.setPen(pen)
+            painter.setBrush(QColor(Qt.white))
+            painter.drawText(QRect(40+deltadistance+(len(self.Source_Failed_dict))/screenwidth * factor+30, 180, (self.same_success/screenwidth) * factor, 100),Qt.AlignCenter,str(self.same_success))
+        distance3 =  distance2 + ((self.same_success/screenwidth) * factor)+20
+        
+        if self.failed!=0:
+            pen.setColor(QColor(Qt.darkRed))
+            painter.setPen(pen)
+            painter.setBrush(QColor(Qt.darkRed))
+            painter.drawRoundedRect(40+deltadistance+(len(self.Source_Failed_dict))/screenwidth * factor+30+(self.same_success/screenwidth) * factor+10, 180, (self.failed/screenwidth) * factor, 100, 10, 10)
+            pen.setColor(QColor(Qt.white))
+            painter.setPen(pen)
+            painter.setBrush(QColor(Qt.white))
+            painter.drawText(QRect(40+deltadistance+(len(self.Source_Failed_dict))/screenwidth * factor+30+(self.same_success/screenwidth) * factor+10, 180, (self.failed/screenwidth) * factor, 100),Qt.AlignCenter,str(self.failed))
+        
+        if self.target_others!=0:
+            pen.setColor(QColor(Qt.darkBlue))
+            painter.setPen(pen)
+            painter.setBrush(QColor(Qt.darkBlue))
+            painter.drawRoundedRect(40+deltadistance+(len(self.Source_Failed_dict))/screenwidth * factor+30+(self.same_success/screenwidth) * factor+10+(self.failed/screenwidth) * factor+30,180,(self.target_others/screenwidth)*factor,100,10,10)
+            pen.setColor(QColor(Qt.white))
+            painter.setPen(pen)
+            painter.setBrush(QColor(Qt.darkBlue))
+            painter.drawText(QRect(40+deltadistance+(len(self.Source_Failed_dict))/screenwidth * factor+30+(self.same_success/screenwidth) * factor+10+(self.failed/screenwidth) * factor+30,180,(self.target_others/screenwidth)*factor,100),Qt.AlignCenter,str(self.target_others))
+
+        pen.setColor(QColor(Qt.darkRed))
+        painter.setPen(pen)
+        painter.setBrush(QColor(Qt.darkRed))
+        painter.drawRect(350,400,10,10)
+
+        pen.setColor(QColor(Qt.darkGreen))
+        painter.setPen(pen)
+        painter.setBrush(QColor(Qt.darkGreen))
+        painter.drawRect(500,400,10,10)
+
+        pen.setColor(QColor(Qt.darkBlue))
+        painter.setPen(pen)
+        painter.setBrush(QColor(Qt.darkBlue))
+        painter.drawRect(660,400,10,10)
+
+        painter.end()
+        
 
 if __name__ == '__main__':
     appctxt = ApplicationContext()
